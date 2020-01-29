2894ddcd9f3fe58ee7027988ebd29ef4b9e93365
Predictive Works <info@predictiveworks.eu>
2020-01-29 13:08:35 +0100

    Diff AutoRegression added

---
+++ works-ts/src/main/scala/de/kp/works/ts/model/DiffAutoRegression.scala
@@ -0,0 +1,181 @@
+package de.kp.works.ts.model
+/*
+ * Copyright (c) 2019 Dr. Krusche & Partner PartG. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * @author Stefan Krusche, Dr. Krusche & Partner PartG
+ * 
+ */
+
+import com.suning.spark.ts.{DiffAutoRegression => SuningDiffAutoRegression}
+import com.suning.spark.regression.{LinearRegression => SuningRegression}
+
+import org.apache.hadoop.fs.Path
+
+import org.apache.spark.ml._
+import org.apache.spark.ml.linalg.Vector
+
+import org.apache.spark.ml.param._
+import org.apache.spark.ml.param.shared._
+
+import org.apache.spark.ml.util._
+
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
+
+trait DiffAutoRegressionParams extends ModelParams with HasPParam with HasDParam 
+      with HasRegParam with HasElasticNetParam 
+      with HasStandardizationParam with HasFitInterceptParam {
+  
+}
+
+class DiffAutoRegression(override val uid: String)
+  extends Estimator[DiffAutoRegressionModel] with DiffAutoRegressionParams with DefaultParamsWritable {
+
+  def this() = this(Identifiable.randomUID("DiffAutoRegression"))
+
+  override def fit(dataset:Dataset[_]):DiffAutoRegressionModel = {
+
+    require($(p) > 0 && $(d) > 0, s"Parameters p, d must be positive")
+ 
+    val suning = SuningDiffAutoRegression(
+      $(valueCol), $(timeCol), $(p), $(d),
+      $(regParam), $(standardization), $(elasticNetParam), $(fitIntercept))
+
+    val model = suning.fit(dataset.toDF)
+
+    val intercept = model.getIntercept
+    val weights = model.getWeights
+
+    copyValues(new DiffAutoRegressionModel(uid, intercept, weights).setParent(this))
+
+  }
+
+  override def transformSchema(schema:StructType):StructType = {
+    schema
+  }
+
+  override def copy(extra:ParamMap):DiffAutoRegression = defaultCopy(extra)
+  
+}
+
+class DiffAutoRegressionModel(override val uid:String, intercept:Double, weights:Vector)
+  extends Model[DiffAutoRegressionModel] with DiffAutoRegressionParams with MLWritable {
+
+  import DiffAutoRegressionModel._
+
+  def getIntercept:Double = intercept
+  
+  def getWeights:Vector = weights
+
+  def this(intercept:Double, weights:Vector) = {
+    this(Identifiable.randomUID("DiffAutoRegressionModel"), intercept, weights)
+  }
+  override def transform(dataset:Dataset[_]):DataFrame = {
+
+    val diffAR = SuningDiffAutoRegression($(valueCol), $(timeCol), $(p), $(d),
+      $(regParam), $(standardization), $(elasticNetParam), $(fitIntercept))
+    
+    val prepared = diffAR.prepareDiffAR(dataset.toDF)
+    val featureCols = diffAR.getFeatureCols
+    
+    val intercept = getIntercept
+    val weights = getWeights
+    
+    val model = LinearRegressionModel.get(intercept,weights)    
+
+    val linearReg = new SuningRegression(featureCols)
+    linearReg.setModel(model)
+    
+    linearReg.transform(prepared)
+
+  }
+
+  override def transformSchema(schema:StructType):StructType = {
+    schema
+  }
+
+  override def copy(extra:ParamMap):DiffAutoRegressionModel = {
+    val copied = new DiffAutoRegressionModel(uid, intercept, weights).setParent(parent)
+    copyValues(copied, extra)
+  }
+
+  override def write: MLWriter = new DiffAutoRegressionModelWriter(this)
+
+}
+
+object DiffAutoRegressionModel extends MLReadable[DiffAutoRegressionModel] {
+
+  private case class Data(intercept: Double, coefficients: Vector)
+  
+  class DiffAutoRegressionModelWriter(instance: DiffAutoRegressionModel) extends MLWriter {
+
+    override def save(path:String): Unit = {
+      super.save(path)
+    }
+    
+    override def saveImpl(path: String): Unit = {
+      
+      /* Save metadata & params */
+      SparkParamsWriter.saveMetadata(instance, path, sc)
+      
+      /* 
+       * Save intercept & weight of the underlying 
+       * linear regression model
+       */
+      val intercept = instance.getIntercept
+      val coefficients = instance.getWeights
+      
+      val data = Data(intercept, coefficients)
+      val dataPath = new Path(path, "data").toString
+      
+      sparkSession.createDataFrame(Seq(data)).repartition(1).write.parquet(dataPath)      
+      
+    }
+    
+  }
+
+  private class DiffAutoRegressionModelReader extends MLReader[DiffAutoRegressionModel] {
+
+    private val className = classOf[DiffAutoRegressionModel].getName
+
+    override def load(path: String):DiffAutoRegressionModel = {
+      
+      /* Read metadata & params */
+      val metadata = SparkParamsReader.loadMetadata(path, sc, className)
+      /*
+       * Retrieve intercept & weight of the underlying
+       * linear regression model
+       */
+      val sparkSession = super.sparkSession
+      import sparkSession.implicits._
+
+      val dataPath = new Path(path, "data").toString
+      val data: Data =  sparkSession.read.parquet(dataPath).as[Data].collect.head
+      /*
+       * Reconstruct trained model instance
+       */
+      val model = new DiffAutoRegressionModel(metadata.uid, data.intercept, data.coefficients)
+      SparkParamsReader.getAndSetParams(model, metadata)
+      
+      model
+      
+    }
+  }
+
+  override def read: MLReader[DiffAutoRegressionModel] = new DiffAutoRegressionModelReader
+
+  override def load(path: String): DiffAutoRegressionModel = super.load(path)
+  
+}
---
+++ works-ts/src/main/scala/de/kp/works/ts/model/ModelParams.scala
@@ -0,0 +1,193 @@
+package de.kp.works.ts.model
+/*
+ * Copyright (c) 2019 Dr. Krusche & Partner PartG. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * @author Stefan Krusche, Dr. Krusche & Partner PartG
+ * 
+ */
+import org.apache.spark.ml.param._
+import org.apache.spark.ml.param.shared._
+
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+/** ARIMA PARAMETERS **/
+
+trait HasStandardizationParam extends Params {
+
+  final val standardization: BooleanParam = new BooleanParam(this, "standardization", "")
+
+  final def getStandardization: Boolean = $(standardization)
+
+  final def setStandardization(value:Boolean): this.type = set(standardization, value)
+  
+}
+
+trait HasFitInterceptParam extends Params {
+
+  final val fitIntercept: BooleanParam = new BooleanParam(this, "fitIntercept", "")
+
+  final def getFitIntercept: Boolean = $(fitIntercept)
+
+  final def setFitIntercept(value:Boolean): this.type = set(fitIntercept, value)
+  
+}
+
+trait HasMeanOutParam extends Params {
+
+  final val meanOut: BooleanParam = new BooleanParam(this, "meanOut", "")
+
+  final def getMeanOut: Boolean = $(meanOut)
+
+  final def setMeanOut(value:Boolean): this.type = set(meanOut, value)
+  
+}
+
+trait HasEarlyStopParam extends Params {
+
+  final val earlyStop: BooleanParam = new BooleanParam(this, "earlyStop", "")
+
+  final def getEarlyStop: Boolean = $(earlyStop)
+
+  final def setEarlyStop(value:Boolean): this.type = set(earlyStop, value)
+  
+}
+
+trait HasCriterionParam extends Params {
+
+  final val criterion: Param[String] = new Param[String](this, "criterion", "", 
+      ParamValidators.inArray(Array("aic", "bic", "aicc")))
+
+  final def getCriterion: String = $(criterion)
+
+  final def setCriterion(value:String): this.type = set(criterion, value)
+  
+}
+
+trait HasPParam extends Params {
+  /**
+   * ARIMA p parameter: order of the autoregressive part.
+   */
+  final val p: IntParam = new IntParam(this, "p", "Order of the autoregressive part. Must be >= 0. "
+      + "Must also be greater than q (order of the moving average part).", ParamValidators.gtEq(0))
+
+  final def getP: Int = $(p)
+
+  final def setP(value:Int): this.type = set(p, value)
+  
+}
+
+trait HasQParam extends Params {  
+  /**
+   * ARIMA q parameter: order of the moving average part.
+   */
+  final val q: IntParam = new IntParam(this, "q", "Order of the moving average part. Must be >= 0. "
+      + "Must be smaller that p (order of the autoregressive part).", ParamValidators.gtEq(0))
+
+  final def getQ: Int = $(q)
+
+  final def setQ(value:Int): this.type = set(q, value)
+  
+}
+
+trait HasDParam extends Params {  
+  /**
+   * ARIMA d parameter: degree of first differencing involved.
+   */
+  final val d: IntParam = new IntParam(this, "d", "Degree of first differencing involved.", ParamValidators.gtEq(0))
+
+  final def getD: Int = $(d)
+
+  final def setD(value:Int): this.type = set(d, value)
+  
+}
+
+trait HasPMaxParam extends Params {
+
+  final val pmax: IntParam = new IntParam(this, "pmax", "The maximum order of the autoregressive part. Must be >= 0. "
+      + "Must also be greater than qmax (maximum order of the moving average part).", ParamValidators.gtEq(0))
+
+  final def getPMax: Int = $(pmax)
+
+  final def setPMax(value:Int): this.type = set(pmax, value)
+  
+}
+
+trait HasQMaxParam extends Params {  
+
+  final val qmax: IntParam = new IntParam(this, "qmax", "Maximum order of the moving average part. Must be >= 0. "
+      + "Must be smaller that pmax (order of the autoregressive part).", ParamValidators.gtEq(0))
+
+  final def getQMax: Int = $(qmax)
+
+  final def setQMax(value:Int): this.type = set(qmax, value)
+  
+}
+
+trait HasDMaxParam extends Params {  
+
+  final val dmax: IntParam = new IntParam(this, "dmax", "Maximum degree of first differencing involved.", ParamValidators.gtEq(0))
+
+  final def getDMax: Int = $(dmax)
+
+  final def setDMax(value:Int): this.type = set(dmax, value)
+  
+}
+
+/** REGRESSION PARAMETERS **/
+
+trait HasRegParam extends Params {
+  /**
+   * Param for regularization parameter (&gt;= 0).
+   */
+  final val regParam: DoubleParam = new DoubleParam(this, "regParam", "regularization parameter (>= 0)", ParamValidators.gtEq(0))
+
+  final def getRegParam: Double = $(regParam)
+  
+  final def setRegParam(value:Double): this.type = set(regParam, value)
+  
+}
+
+trait HasElasticNetParam extends Params {
+
+  /**
+   * Param for the ElasticNet mixing parameter, in range [0, 1]. For alpha = 0, 
+   * the penalty is an L2 penalty. For alpha = 1, it is an L1 penalty.
+   */
+  final val elasticNetParam: DoubleParam = new DoubleParam(this, "elasticNetParam", "the ElasticNet mixing parameter, "
+      + "in range [0, 1]. For alpha = 0, the penalty is an L2 penalty. For alpha = 1, it is an L1 penalty", ParamValidators.inRange(0, 1))
+
+  final def getElasticNetParam: Double = $(elasticNetParam)
+
+  final def setElasticNetParam(value:Double): this.type = set(elasticNetParam, value)
+
+}
+
+trait ModelParams extends Params {
+  
+  final val timeCol = new Param[String](this, "timeCol",
+      "Name of the timestamp field", (value:String) => true)
+   
+  final val valueCol = new Param[String](this, "valueCol",
+      "Name of the value field", (value:String) => true)
+ 
+  /** @group setParam */
+  def setTimeCol(value:String): this.type = set(timeCol, value)
+ 
+  /** @group setParam */
+  def setValueCol(value:String): this.type = set(valueCol, value)
+  
+}
