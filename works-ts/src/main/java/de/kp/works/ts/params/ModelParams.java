340260ee12276c5803d304d832cd246a40aaa110
Predictive Works <info@predictiveworks.eu>
2020-01-29 17:06:07 +0100

    works-ts module updated

---
+++ works-ts/src/main/java/de/kp/works/ts/params/ModelParams.java
@@ -0,0 +1,55 @@
+package de.kp.works.ts.params;
+/*
+ * Copyright (c) 2019 Dr. Krusche & Partner PartG. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * @author Stefan Krusche, Dr. Krusche & Partner PartG
+ * 
+ */
+
+public class ModelParams {
+
+	public static final String CRITERION_DESC = "";
+
+	public static final String EARLY_STOP_DESC = "";
+
+	public static final String FIT_INTERCEPT_DESC = "";
+
+	public static final String MEAN_OUT_DESC = "";
+
+	public static final String P_DESC = "";
+
+	public static final String P_MAX_DESC = "";
+
+	public static final String Q_DESC = "";
+
+	public static final String Q_MAX_DESC = "";
+
+	public static final String D_DESC = "";
+
+	public static final String D_MAX_DESC = "";
+
+	public static final String ELASTIC_NET_PARAM_DESC = "";
+
+	public static final String REG_PARAM_DESC = "";
+	
+	public static final String STANDARDIZATION_DESC = "";
+
+	public static final String GROUP_COL_DESC = "";
+	
+	public static final String TIME_COL_DESC = "";
+	
+	public static final String VALUE_COL_DESC = "";
+	
+}
