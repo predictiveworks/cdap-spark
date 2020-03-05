7fb6f2fcf87bc29cfbf78f17db4e13fca316a8b7
Predictive Works <info@predictiveworks.eu>
2020-03-05 18:14:32 +0100

    Model management updated

---
+++ works-core/src/main/java/de/kp/works/core/Names.java
@@ -0,0 +1,40 @@
+package de.kp.works.core;
+/*
+ * Copyright (c) 2019 Dr. Krusche & Partner PartG. All rights reserved.
+ *
+ * This software is the confidential and proprietary information of 
+ * Dr. Krusche & Partner PartG ("Confidential Information"). 
+ * 
+ * You shall not disclose such Confidential Information and shall use 
+ * it only in accordance with the terms of the license agreement you 
+ * entered into with Dr. Krusche & Partner PartG.
+ * 
+ * @author Stefan Krusche, Dr. Krusche & Partner PartG
+ * 
+ */
+
+public class Names {
+
+	public static final String ACCURACY = "accuracy";
+	public static final String F1 = "f1";
+	public static final String HAMMING_LOSS = "hammingLoss";
+	public static final String WEIGHTED_FMEASURE = "weightedFMeasure";
+	public static final String WEIGHTED_PRECISION = "weightedPrecision";
+	public static final String WEIGHTED_RECALL = "weightedRecall";
+	public static final String WEIGHTED_FALSE_POSITIVE = "weightedFalsePositiveRate";
+	public static final String WEIGHTED_TRUE_POSITIVE = "weightedTruePositiveRate";	
+
+	public static final String RSME = "rsme";
+	public static final String MSE = "mse";
+	public static final String MAE = "mae";
+	public static final String R2 = "r2";
+	
+	public static final String SILHOUETTE_EUCLDIAN = "silhouette_euclidean";
+	public static final String SILHOUETTE_COSINE = "silhouette_cosine";
+	public static final String PERPLEXITY = "perplexity";
+	public static final String LIKELIHOOD = "likelihood";
+	
+	public static final String FS_PATH = "fsPath";
+	public static final String TIMESTAMP = "timestamp";
+	
+}
--- works-core/src/main/java/de/kp/works/core/model/ModelScanner.java
+++ works-core/src/main/java/de/kp/works/core/model/ModelScanner.java
@@ -24,103 +24,192 @@ import java.util.List;
 import co.cask.cdap.api.dataset.table.Row;
 import co.cask.cdap.api.dataset.table.Scanner;
 import co.cask.cdap.api.dataset.table.Table;
+import de.kp.works.core.Algorithms;
 
 public class ModelScanner {
 
 	public String bestClassifier(Table table, String algoName, String modelName, String modelStage) {
+
+		String fsPath = null;
 		/*
-		 * All classifiers are evaluated leveraging the
-		 * same evaluator, i.e. no distinction between
-		 * different algorithms is requied
+		 * All classifiers are evaluated leveraging the same evaluator, i.e. no
+		 * distinction between different algorithms is requied
 		 */
 		List<ClassifierMetric> metrics = new ArrayList<>();
 		Row row;
 
 		Scanner rows = table.scan(null, null);
 		while ((row = rows.next()) != null) {
-			
+
 			String algorithm = row.getString("algorithm");
 			if (algorithm.equals(algoName)) {
 
 				String name = row.getString("name");
 				if (name.equals(modelName)) {
 
-					ClassifierMetric metric = new ClassifierMetric();
-					metric.fromRow(row);
-					
-					metrics.add(metric);
-					
+					String stage = row.getString("stage");
+					if (stage.equals(modelStage)) {
+
+						ClassifierMetric metric = new ClassifierMetric();
+						metric.fromRow(row);
+
+						metrics.add(metric);
+
+					}
 				}
-				
+			}
+
+		}
+
+		fsPath = new ModelFinder().findClassifier(algoName, metrics);
+		return fsPath;
+
+	}
+
+	public String bestCluster(Table table, String algoName, String modelName, String modelStage) {
+
+		String fsPath = null;
+
+		List<ClusterMetric> metrics = new ArrayList<>();
+		Row row;
+
+		Scanner rows = table.scan(null, null);
+		while ((row = rows.next()) != null) {
+
+			String algorithm = row.getString("algorithm");
+			if (algorithm.equals(algoName)) {
+
+				String name = row.getString("name");
+				if (name.equals(modelName)) {
+
+					String stage = row.getString("stage");
+					if (stage.equals(modelStage)) {
+
+						ClusterMetric metric = new ClusterMetric();
+						metric.fromRow(row);
+
+						metrics.add(metric);
+
+					}
+				}
+
 			}
 
 		}
 		
-		// TODO
+		fsPath = new ModelFinder().findCluster(algoName, metrics);
+		return fsPath;
 		
-		return null;
-	}
-
-	public String bestCluster(Table table, String algoName, String modelName, String modelStage) {
-		// TODO
-		return null;
 	}
 
 	public String bestFeature(Table table, String algoName, String modelName, String modelStage) {
 		/*
-		 * Feature models do not have any evaluation
-		 * metric assign
+		 * Feature models do not have any evaluation metric assign
 		 */
 		throw new IllegalArgumentException("Determine the best feature model is not supported.");
 
 	}
 
 	public String bestRecommender(Table table, String algoName, String modelName, String modelStage) {
-		// TODO
-		return null;
+
+		String fsPath = null;
+		switch (algoName) {
+		case Algorithms.ALS: {
+			fsPath = bestRegressor(table, algoName, modelName, modelStage);
+			break;
+		}
+		default:
+			throw new IllegalArgumentException(
+					String.format("Searching for the best recommender model is not supported for : %s.", algoName));
+		}
+
+		return fsPath;
+
 	}
 
 	public String bestRegressor(Table table, String algoName, String modelName, String modelStage) {
+
+		String fsPath = null;
 		/*
-		 * All regressors are evaluated leveraging the
-		 * same evaluator, i.e. no distinction between
-		 * different algorithms is requied
+		 * All regressors are evaluated leveraging the same evaluator, i.e. no
+		 * distinction between different algorithms is requied
 		 */
 		List<RegressorMetric> metrics = new ArrayList<>();
 		Row row;
 
 		Scanner rows = table.scan(null, null);
 		while ((row = rows.next()) != null) {
-			
+
 			String algorithm = row.getString("algorithm");
 			if (algorithm.equals(algoName)) {
 
 				String name = row.getString("name");
 				if (name.equals(modelName)) {
 
-					RegressorMetric metric = new RegressorMetric();
-					metric.fromRow(row);
-					
-					metrics.add(metric);
-					
+					String stage = row.getString("stage");
+					if (stage.equals(modelStage)) {
+
+						RegressorMetric metric = new RegressorMetric();
+						metric.fromRow(row);
+
+						metrics.add(metric);
+
+					}
 				}
-				
+
 			}
 
 		}
 		
-		return null;
+		fsPath = new ModelFinder().findRegressor(algoName, metrics);
+		return fsPath;
+		
 	}
 
 	public String bestText(Table table, String algoName, String modelName, String modelStage) {
-		
-		// TODO
-		return null;
+
+		String fsPath = null;
+		switch (algoName) {
+
+		case Algorithms.VIVEKN_SENTIMENT: {
+			fsPath = bestRegressor(table, algoName, modelName, modelStage);
+			break;
+		}
+		default:
+			throw new IllegalArgumentException(
+					String.format("Searching for best natural language model is not supported for : %s.", algoName));
+		}
+
+		return fsPath;
 	}
 
 	public String bestTime(Table table, String algoName, String modelName, String modelStage) {
-		
-		// TODO
-		return null;
+
+		String fsPath = null;
+		switch (algoName) {
+		case Algorithms.ACF: {
+			throw new IllegalArgumentException(
+					String.format("Searching for best time series model is not supported for : %s.", algoName));
+		}
+		case Algorithms.AR:
+		case Algorithms.ARIMA:
+		case Algorithms.ARMA:
+		case Algorithms.AUTO_AR:
+		case Algorithms.AUTO_ARIMA:
+		case Algorithms.AUTO_ARMA:
+		case Algorithms.AUTO_MA:
+		case Algorithms.DIFF_AR:
+		case Algorithms.MA:
+		case Algorithms.RANDOM_FOREST_TREE:
+		case Algorithms.YULE_WALKER: {
+			fsPath = bestRegressor(table, algoName, modelName, modelStage);
+			break;
+		}
+		default:
+			throw new IllegalArgumentException(
+					String.format("Searching for best time series model is not supported for : %s.", algoName));
+		}
+
+		return fsPath;
 	}
 }
